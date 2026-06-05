5b6b1fa41886aae85aee070062fa2cf5ddaf01f4
auraecosystem <sandraisom@icloud.com>
2026-06-05 02:36:57 +0100

    Delete client/Main.d

--- client/Main.d
+++
@@ -1,9 +0,0 @@
-// DEPRECATED: client/Main.d
-// This file has been replaced by client/main.dart which contains the new
-// application entrypoint and the MoviesFutureBuilder widget.
-//
-// The original file was left in place to avoid breaking references; please
-// update any references to import 'client/main.dart' instead of this file.
-
-// NOTE: This file is intentionally left as a deprecation notice. The
-// functional implementation now lives in client/main.dart.
