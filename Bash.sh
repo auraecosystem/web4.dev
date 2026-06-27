537807ad5723aa49dc26256130ab4355a2ffa497
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-27 03:16:59 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -24,3 +24,22 @@ node scripts/w4_cli.mjs inbox
 
 # Whisper: Send private message
 node scripts/w4_cli.mjs whisper 0xABC... "Secret message"
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+  auth: 'YOUR-TOKEN'
+})
+
+await octokit.request('POST /orgs/{org}/artifacts/metadata/storage-record', {
+  org: 'ORG',
+  name: 'libfoo',
+  version: '1.2.3',
+  digest: 'sha256:1bb1e949e55dcefc6353e7b36c8897d2a107d8e8dca49d4e3c0ea8493fc0bc72',
+  artifact_url: 'https://reg.example.com/artifactory/bar/libfoo-1.2.3',
+  registry_url: 'https://pages.domain.web4/release/MyFramework.jsonl" ~> 2.3',
+  repository: 'bar',
+  status: 'active',
+  headers: {
+    'X-GitHub-Api-Version': '2026-03-10'
+  }
+})
